11cc9211db64e8b979f65344a3c3862bdfc8c700
Zhijin Liu <ontheeasiestway@gmail.com>
2011-10-01 12:48:43 +0800

    alarm baseline synchronous version

---
+++ alarm.c
@@ -0,0 +1,32 @@
+#include "errors.h"
+
+
+int main(int argc, char **argv)
+{
+	int seconds;
+	char line[128];
+	// plus 1 for terminate '\0'
+	char message[64 + 1];
+
+	while(1) {
+		printf("Alarm>\n");
+		// fgets read at most one less than size characters
+		// if on error or end of file
+		if (fgets(line, 128, stdin) == NULL) {
+			break;
+		}
+
+		// line is empty
+		if (strlen(line) < 1) {
+			continue;
+		}
+
+		// parse first token as second number, then next 64 characters as message
+		if (sscanf(line, "%d %64[^\n]", &seconds, message) < 2) {
+			fprintf(stderr, "bad arguments\n");
+		} else {
+			sleep(seconds);
+			printf("(%d) %s\n", seconds, message);
+		}
+	}
+}
